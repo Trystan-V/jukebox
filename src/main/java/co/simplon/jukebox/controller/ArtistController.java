99c9374735fe65ad1440201dbd7eab3282b0da8d
Trystan VALENTIN <trystan.valentin@gmail.com>
2020-12-01 16:38:27 +0100

    problème avec ArtistService

--- src/main/java/co/simplon/jukebox/controller/ArtistController.java
+++ src/main/java/co/simplon/jukebox/controller/ArtistController.java
@@ -1,23 +1,65 @@
 package co.simplon.jukebox.controller;
 
 import co.simplon.jukebox.Artist;
+import co.simplon.jukebox.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jukebox")
-public class ArtistController {
+public class ArtistController<ArtistService> {
+
+    @Autowired
+    ArtistService service;
+    @CrossOrigin
+    @GetMapping("/artists")
+    public ResponseEntity<List<Artist>> getAllArtist(@RequestParam(value="search", defaultValue="") String search) {
+        List<Artist> listArtist;
+        try {
+            listArtist = service.findAll(search);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(listArtist);
+    }
 
     @CrossOrigin
-    @GetMapping("/artist/hello")
-    ResponseEntity<Artist> getArtistToto() {
-        Artist hello = new Artist();
-        hello.setName("Hello");
-        hello.setBio("Comment allez-vous ?");
-        hello.setFanNumber(100);
-        return ResponseEntity.ok().body(hello);
+    @GetMapping("/artists/{id}")
+    ResponseEntity<Artist> getArtistById(@PathVariable(value="id") long id) {
+        Optional<Artist> artist = service.findById(id);
+        if (artist.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(artist.get());
+    }
+    
+    @PostMapping("/artists")
+    ResponseEntity<Artist> addArtist(@Valid @RequestBody Artist artist){
+        return ResponseEntity.ok().body(service.insert(artist));
+    }
+
+    @CrossOrigin
+    @PutMapping("/artists/{id}")
+    ResponseEntity<Artist> updateArtiste(@PathVariable(value="id") long id, @Valid @RequestBody Artist artist){
+        Artist updatedArtiste = service.update(id, artist);
+        if(updatedArtiste == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(updatedArtiste);
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/artists/{id}")
+    ResponseEntity<Artist> deleteArtist(@PathVariable(value="id") long id){
+        Optional<Artist> artist = service.findById(id);
+        if(artist.isEmpty())
+            return ResponseEntity.notFound().build();
+        service.delete(artist.get().getId());
+        return ResponseEntity.accepted().build();
     }
 }
---
+++ src/main/java/co/simplon/jukebox/service/ArtistServiceImpl.java
@@ -0,0 +1,60 @@
+package co.simplon.jukebox.service;
+
+import co.simplon.jukebox.Artist;
+import co.simplon.jukebox.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ArtistServiceImpl implements ArtistService {
+
+    @Autowired
+    private ArtistRepository repository;
+
+    @Override
+    public List<Artist> findAll(String search) {
+        if(!"".equals(search))
+            return repository.findByNameContaining(search);
+        else
+            return repository.findAll();
+    }
+
+    @Override
+    public Optional<Artist> findById (Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Artist insert(Artist artist) {
+        return repository.save(artist);
+    }
+
+    @Override
+    public Artist update(Long id, Artist artist) {
+
+        Optional<Artist> optionalArtist = this.findById(id);
+
+        if(optionalArtist.isPresent()) {
+            Artist artistToUpdate = optionalArtist.get();
+            artistToUpdate.setName(artist.getName());
+            if (artist.getBio() != null)
+                artistToUpdate.setBio(artist.getBio());
+            if (artist.getFanNumber() != null)
+                artistToUpdate.setFanNumber(artist.getFanNumber());
+            return repository.save(artistToUpdate);
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+        Optional<Artist> artist = this.findById(id);
+        if (artist.isPresent()) {
+            repository.delete(artist.get());
+        }
+    }
+
+}
